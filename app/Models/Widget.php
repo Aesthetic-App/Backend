9040c410a0227b21d5aa3148ff732bf230bcacfc
eripserkan <serkanerip@gmail.com>
2021-07-09 13:44:16 +0300

    Append background image

--- app/Models/Widget.php
+++ app/Models/Widget.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
@@ -17,6 +18,14 @@ class Widget extends Model implements HasMedia
         'theme_hex_color', 'background_hex_color', 'font_name', 'text'
     ];
 
+    protected $appends = [
+        'background_image'
+    ];
+
+    private $excludedFieldsToArray = [
+        'media'
+    ];
+
     public function widget_type()
     {
         return $this->belongsTo(WidgetType::class, "type_id");
@@ -25,5 +34,16 @@ class Widget extends Model implements HasMedia
     public function widget_category()
     {
         return $this->belongsTo(WidgetCategory::class, "category_id");
+    }
+
+    public function getBackgroundImageAttribute()
+    {
+        return $this->getFirstMediaUrl("background_images");
+    }
+
+    public function toArray()
+    {
+        $array = parent::toArray();
+        return Arr::except($array, $this->excludedFieldsToArray);
     }
 }
