bdc8589940a08ac9cdb6f8e3d7099e4373ebe0cb
eripserkan <serkanerip@gmail.com>
2021-08-10 11:20:33 +0300

    Fix

--- app/Http/Controllers/Api/HeadlineController.php
+++ app/Http/Controllers/Api/HeadlineController.php
@@ -21,7 +21,7 @@ class HeadlineController extends Controller
                 $catArray[] = [
                     "id" => $category->id,
                     "title" => $category->title,
-                    "is_premium" => $category->title,
+                    "is_premium" => $category->premium,
                     "cover_image_url" => $category->cover_image_url,
                 ];
             }
