6788204313f882fd20ddb8b52b27abbdd241abfa
eripserkan <serkanerip@gmail.com>
2021-09-24 00:09:55 +0300

    Add missing languages

--- nova-components/Translation/src/config/config.php
+++ nova-components/Translation/src/config/config.php
@@ -11,8 +11,120 @@ return [
             'language' => 'English'
         ],
         [
-            'code' => 'fr',
-            'language' => 'French'
+            'code' => 'ca',
+            'language' => 'Catalan'
+        ],
+        [
+            'code' => 'hr',
+            'language' => 'Crotian'
+        ],
+        [
+            'code' => 'cs',
+            'language' => 'Czech'
+        ],
+        [
+            'code' => 'da',
+            'language' => 'Danish'
+        ],
+        [
+            'code' => 'nl',
+            'language' => 'Dutch'
+        ],
+        [
+            'code' => 'fi',
+            'language' => 'Finnish'
+        ],
+        [
+            'code' => 'de',
+            'language' => 'German'
+        ],
+        [
+            'code' => 'el',
+            'language' => 'Greek'
+        ],
+        [
+            'code' => 'hi',
+            'language' => 'Hindi'
+        ],
+        [
+            'code' => 'hu',
+            'language' => 'Hungarian'
+        ],
+        [
+            'code' => 'id',
+            'language' => 'Indonesian'
+        ],
+        [
+            'code' => 'it',
+            'language' => 'Italian'
+        ],
+        [
+            'code' => 'ja',
+            'language' => 'Japanese'
+        ],
+        [
+            'code' => 'ko',
+            'language' => 'Korean'
+        ],
+        [
+            'code' => 'ms',
+            'language' => 'Malay'
+        ],
+        [
+            'code' => 'es-MX',
+            'language' => 'Mexican Spanish'
+        ],
+        [
+            'code' => 'nb',
+            'language' => 'Norwegian'
+        ],
+        [
+            'code' => 'pl',
+            'language' => 'Polish'
+        ],
+        [
+            'code' => 'pt',
+            'language' => 'Portuguese'
+        ],
+        [
+            'code' => 'ro',
+            'language' => 'Romanian'
+        ],
+        [
+            'code' => 'ru',
+            'language' => 'Russian'
+        ],
+        [
+            'code' => 'zh-CN',
+            'language' => 'Simplified Chinese'
+        ],
+        [
+            'code' => 'sk',
+            'language' => 'Slovak'
+        ],
+        [
+            'code' => 'es',
+            'language' => 'Spanish'
+        ],
+        [
+            'code' => 'sv',
+            'language' => 'Swedish'
+        ],
+        [
+            'code' => 'th',
+            'language' => 'Thai'
+        ],
+        [
+            'code' => 'zh-TW',
+            'language' => 'Traditional Chinese'
+        ],
+        [
+            'code' => 'uk',
+            'language' => 'Ukrainian'
+        ],
+        [
+            'code' => 'vi',
+            'language' => 'Vietnamese'
         ],
     ]
 ];
