1e7cc92465b5a2dc7cc6bf722d781230a74fea2a
eripserkan <serkanerip@gmail.com>
2021-10-04 21:52:51 +0300

    Change translation messages response

--- app/Http/Controllers/Api/TranslationMessagesApiController.php
+++ app/Http/Controllers/Api/TranslationMessagesApiController.php
@@ -4,18 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\TranslationMessage;
-use Illuminate\Http\Request;
 
 class TranslationMessagesApiController extends Controller
 {
     public function locale(string $locale) {
-        $messages = [];
-        foreach (TranslationMessage::all() as $messageModel) {
-
-            $message = $messageModel->messages[$locale] ?? ($messageModel->messages['en'] ?? '');
-            $messages[$messageModel->key] = $message;
-        }
-
-        return $messages;
+        return TranslationMessage::all()
+            ->map(fn($messageModel) => [
+                'key' => $messageModel->key,
+                'value' => $messageModel->messages[$locale] ?? ($messageModel->messages['en'] ?? '')
+            ]);
     }
 }
