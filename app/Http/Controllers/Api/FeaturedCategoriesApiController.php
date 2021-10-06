27c90e99623476aff0bf0bce8abd64abfa906906
eripserkan <serkanerip@gmail.com>
2021-10-06 17:40:13 +0300

    New features

--- app/Http/Controllers/Api/FeaturedCategoriesApiController.php
+++ app/Http/Controllers/Api/FeaturedCategoriesApiController.php
@@ -11,14 +11,35 @@ use OptimistDigital\NovaSettings\Models\Settings;
 
 class FeaturedCategoriesApiController extends Controller
 {
-    public function index()
+    private  function withoutPagination()
     {
         $settings = Settings::all();
         $themeCategoryID = Settings::query()->find(['key' => 'featured_theme_category'])->first();
         $wallpaperCategoryID = Settings::query()->find(['key' => 'featured_wallpaper_category'])->first();
         $widgetCategoryID = Settings::query()->find(['key' => 'featured_widget_category'])->first();
+
         return [
-          'widget' => WidgetCategory::query()
+            'widget' => WidgetCategory::query()
+                ->with("widgets")
+                ->find(['id' => $widgetCategoryID->value ?? 0])->first()->setAppends(['cover_image', 'featured_cover_image']),
+            'wallpaper' => WallpaperCategory::query()
+                ->find(['id' => $wallpaperCategoryID->value ?? 0])->first()->setAppends(['cover_image', 'featured_cover_image', 'images']),
+            'theme' => ThemeCategory::query()
+                ->with('themes')
+                ->find(['id' => $themeCategoryID->value ?? 0])->first()->setAppends(['cover_image', 'featured_cover_image']),
+        ];
+    }
+    public function index(Request $request)
+    {
+        if ((bool)$request->get('without_pagination', false) === true) {
+            return $this->withoutPagination();
+        }
+        $settings = Settings::all();
+        $themeCategoryID = Settings::query()->find(['key' => 'featured_theme_category'])->first();
+        $wallpaperCategoryID = Settings::query()->find(['key' => 'featured_wallpaper_category'])->first();
+        $widgetCategoryID = Settings::query()->find(['key' => 'featured_widget_category'])->first();
+        return [
+            'widget' => WidgetCategory::query()
             ->find(['id' => $widgetCategoryID->value ?? 0])->first(),
             'wallpaper' => WallpaperCategory::query()
                 ->find(['id' => $wallpaperCategoryID->value ?? 0])->first(),
