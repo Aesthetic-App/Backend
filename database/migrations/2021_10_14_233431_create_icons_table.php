486d48bb9da340d1692595975d6edd78fe2f06d1
eripserkan <serkanerip@gmail.com>
2021-10-15 03:37:12 +0300

    Create icons table

---
+++ database/migrations/2021_10_14_233431_create_icons_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Icon;
+use App\Models\Theme;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class CreateIconsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('icons', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("theme_id");
+            $table->string("name");
+            $table->unsignedInteger("sort_order")
+                ->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('icons');
+    }
+}
