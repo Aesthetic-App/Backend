612b8f42c01cf1ee0d5a67cd1f94e1c54c1e3535
eripserkan <serkanerip@gmail.com>
2021-10-20 22:28:45 +0300

    Create wallpapers migration

---
+++ database/migrations/2021_10_20_185612_create_wallpapers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWallpapersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallpapers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("wallpaper_category_id");
+            $table->string("name")
+                ->nullable();
+            $table->boolean("is_premium")
+                ->default(false);
+            $table->unsignedInteger("sort_order")
+                ->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallpapers');
+    }
+}
