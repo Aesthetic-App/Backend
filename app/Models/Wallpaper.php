a47ccb12f6b295da4346dd27a719907283273305
eripserkan <serkanerip@gmail.com>
2021-10-20 22:29:06 +0300

    Create wallpaper model

---
+++ app/Models/Wallpaper.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\EloquentSortable\SortableTrait;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class Wallpaper extends Model implements HasMedia
+{
+    use HasFactory;
+    use InteractsWithMedia;
+    use SortableTrait;
+
+    protected $guarded = [];
+
+    protected $hidden = ['media'];
+
+    public $sortable = [
+        'order_column_name' => 'sort_order',
+        'sort_when_creating' => true,
+    ];
+
+    protected $appends = ['image'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('order', function ($builder) {
+            $builder->orderBy('sort_order', 'asc');
+        });
+    }
+
+    public function wallpaper_category(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(WallpaperCategory::class);
+    }
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('thumbnail')
+            ->width(127)
+            ->height(412);
+
+        $this->addMediaConversion('small-image')
+            ->width(130)
+            ->height(130);
+    }
+
+    public function getImageAttribute()
+    {
+        $x = $this->getFirstMedia("wallpapers");
+        return [
+            'normal' => $x ? $x->getFullUrl() : '',
+            'thumbnail' => $x ? $x->getFullUrl('thumbnail') : '',
+        ];
+    }
+}
