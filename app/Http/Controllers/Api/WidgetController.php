f26efaf43092e44cc0621ea6b648092ce6e1e6f9
eripserkan <serkanerip@gmail.com>
2021-11-15 22:00:41 +0300

    Refactor WidgetController

--- app/Http/Controllers/Api/WidgetController.php
+++ app/Http/Controllers/Api/WidgetController.php
@@ -3,49 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Widget;
-use App\Models\WidgetType;
 use Illuminate\Http\Request;
-use App\Models\WidgetCategory;
 use App\Http\Controllers\Controller;
 
 class WidgetController extends Controller
 {
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        $types = [];
-
-        foreach(WidgetType::all() as $type) {
-            $typeArr = [
-                'name' => $type->name,
-                'categories' => []
-            ];
-
-            /** @var WidgetCategory */
-            foreach($type->widget_categories as $category) {
-                $cat = $category->toArray();
-                $cat['widgets'] = $category->widgets;
-                $typeArr['categories'][] = $cat;
-            }
-
-            $types[] = $typeArr;
-        }
-        
-        return $types;
+        return Widget::all();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function categoryImages(Request $request, int $id)
+    public function categoryImages(Request $request, int $id): array
     {
         $widget = Widget::where('category_id', $id)->first();
         $images = [];
